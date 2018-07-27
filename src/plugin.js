cd49d80158b46d42831c89055808bc47c2d8f559
Petr Busliuk <pbusliuk.ctr@virtru.com>
2018-07-27 12:53:07 +0300

    add callback + build plugin

--- src/plugin.js
+++ src/plugin.js
@@ -11,10 +11,12 @@ export default function ({ Plugin, types: t }) {
         visitor: {
             Function (node) {
                 if (!alreadyWrapped(node)) {
-                    node.body = wrap(node.body, function (error) {
-                        console.error('IN babel try catch wrapper, error is :' +
-                            ' ' + error)
-                    });
+                    node.body = wrap(
+                        node.body,
+                        function (error) {
+                            console.error('IN babel try catch wrapper, error:' + error)
+                        }
+                    );
                 }
             }
         }
