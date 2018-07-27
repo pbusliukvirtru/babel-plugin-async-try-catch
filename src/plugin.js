9e5e1c6c8cac4c09a649515f42c4655df39c0198
Petr Busliuk <pbusliuk.ctr@virtru.com>
2018-07-27 15:59:40 +0300

    migrate babel 5 => babel 6

--- src/plugin.js
+++ src/plugin.js
@@ -1,13 +1,13 @@
 import 'source-map-support/register';
 import wrap from './wrap.js';
 
-export default function ({ Plugin, types: t }) {
+export default function ({ types: t }) {
     function alreadyWrapped (node) {
         let body = node.body.body;
         return body && body.length === 1 && t.isTryStatement(body[0]);
     }
 
-    return new Plugin('wrap-functions-in-try-catch', {
+    return {
         visitor: {
             Function (node) {
                 if (!alreadyWrapped(node)) {
@@ -20,5 +20,5 @@ export default function ({ Plugin, types: t }) {
                 }
             }
         }
-    });
+    }
 }
